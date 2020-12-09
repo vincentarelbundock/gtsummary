4c879e52d2d701c53409f0b4fd585a9ff673033e
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-12-09 17:19:16 -0500

    get_coeftest

--- tests/testthat/test-fixest.R
+++ tests/testthat/test-fixest.R
@@ -6,7 +6,8 @@ library(fixest)
 test_that("fixest", {
   mod <- feols(Sepal.Length ~ Sepal.Width + Petal.Length | Species, iris)
   raw <- modelsummary(mod, "data.frame")
-  expect_S3_class(raw, "data.frame")
+  expect_s3_class(raw, "data.frame")
+  expect_equal(dim(raw), c(14, 4))
 })
 
 test_that("fixest glance_custom", {
